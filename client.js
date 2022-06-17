8ad80fed5bc791deb8b9a84b1db8365ca2ac6359
Avery Yang <averyyang2674@gmail.com>
2022-06-17 16:43:33 -0500

    jQuery is working!

--- client.js
+++ client.js
@@ -1,1 +1,9 @@
+//document.ready
+$(ready)
+
+function ready() {
+console.log('Ready')
+}
 // store the information to calculate monthly costs, append information to the DOM and clear the input fields.
+
+// For Base mode, it does **not** need to remove that Employee's salary from the reported total.
