232307e128d483d24586afc09e511450a0935ca3
Avery Yang <averyyang2674@gmail.com>
2022-06-17 19:05:27 -0500

    function add employee

--- client.js
+++ client.js
@@ -1,9 +1,32 @@
 //document.ready
 $(ready)
 
+//ready function
 function ready() {
-console.log('Ready')
+    //get the values from input fields
+    $('#submit-btn').on('click', addEmployee);
 }
+
+//inputs
+let firstName = $('#first-name')
+let lastName = $('#last-name')
+let employeeId = $('#employee-id')
+let jobTitle = $('#job-title')
+let annualSalary = $('#annual-salary')
+let submitBtn = $('#submit-btn')
+
+console.log(lastName.val());
+
 // store the information to calculate monthly costs, append information to the DOM and clear the input fields.
+function addEmployee() {
+    $('tbody').append(`<tr>
+    <td>${firstName.val()}</td>
+    <td>${lastName.val()}</td>
+    <td>${employeeId.val()}</td>
+    <td>${jobTitle.val()}</td>
+    <td>${annualSalary.val()}</td>
+    <td><button id="delete-btn">Remove</button></td>
+</tr>`);
+}
 
 // For Base mode, it does **not** need to remove that Employee's salary from the reported total.
